b01729dd240abc1eaec3f07502e02c26710c709e
ihategoto <cdemilio80@gmail.com>
2020-12-29 23:13:13 +0100

    .

--- modbus_handler/api/app.py
+++ modbus_handler/api/app.py
@@ -1,3 +1,9 @@
-def app(environ, start_fn):
-    start_fn('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes("Hello world!\n", "utf-8")]
+class App(object):
+    def __init__(self, env, start_r):
+        self.env = env
+        self.start = start_r
+
+    def __iter__(self):
+        response_headers = [('Content-type', 'text/plain')]
+        self.start('200 OK', response_headers)
+        yield bytes('Hello world!\n', 'latin-1')
