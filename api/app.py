2558db5bc77be8d49046239e42c6a3341e9f1a3c
ihategoto <cdemilio80@gmail.com>
2020-12-30 00:56:24 +0100

    .

--- api/app.py
+++ api/app.py
@@ -11,7 +11,14 @@ class App(object):
             self.start("405 Method Not Allowed", [('Content-Type','text/plain')])
             yield bytes("GET only method allowed!", "latin-1")
         data = urllib.parse.parse_qs(self.env['QUERY_STRING'])
-        print(repr(data), flush=True)
+        if "slave" not in data or "sensor" not in data or "value" not in data:
+            self.start("400 Bad Request", [('Content-Type','text/plain')])
+            yield bytes("One ore more parameters are missing!", "latin-1")
+        try:
+            Handler.write(data["slave"][0], data["sensor"][0], data["value"][0])
+        except Exception:
+            self.start("500 Internal Server Error", [('Content-Type','text/plain')])
+            yield bytes("Error while writing on sensor!", "latin-1")  
         self.start("200 OK", [('Content-Type','text/plain')])
         yield bytes("OK.\n", "latin-1")
         
