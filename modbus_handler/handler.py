83d186e38d8b237b6a7bb31473abb95d87373b07
ihategoto <cdemilio80@gmail.com>
2020-12-29 18:01:48 +0100

    .

--- modbus_handler/handler.py
+++ modbus_handler/handler.py
@@ -167,7 +167,7 @@ class RefreshThread(object):
     def __init__(self, instance):
         self.instance = instance
         thread = threading.Thread(target = self.run, args = ())
-        thread.daemon = False
+        thread.daemon = True
         thread.start()
 
     def run(self):
