b7eb0469968506f27fcf47fcb5631a4f931c23fb
ihategoto <cdemilio80@gmail.com>
2021-03-01 16:27:21 +0100

    .

--- modbus_handler/handler.py
+++ modbus_handler/handler.py
@@ -115,7 +115,7 @@ class Handler:
                     print("Errore MODBUS durante la lettura di {} da {}: {}".format(sensor['address'], slave['info']['address'], e))
                 except BeanstalkError as e:
                     print("Impossibile scrivere sul server BeansTalk il contenuto del sensore {} dello slave {}: {}".format(sensor['address'], slave['info']['address'], e))
-        slave['instance'].serial.close()   #per dare la possibilità al daemon dei comandi di accedere agli slave.
+        #slave['instance'].serial.close()   #per dare la possibilità al daemon dei comandi di accedere agli slave.
     
     """
     Ritorna l'indirizzo relativo, il functioncode adatto al sensore e la funzione corretta di minimalmodbus.
