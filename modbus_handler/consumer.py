e54c199792400b4322fe71376fbeab635d7b58d7
ihategoto <cdemilio80@gmail.com>
2021-03-01 15:13:41 +0100

    .

--- modbus_handler/consumer.py
+++ modbus_handler/consumer.py
@@ -1,6 +1,9 @@
 import json
 from pystalk import BeanstalkClient, BeanstalkError
 
+BEANSTALKD_HOST = '127.0.0.1'
+BEANSTALKD_PORT = 11300
+
 client = BeanstalkClient(BEANSTALKD_HOST, BEANSTALKD_PORT, auto_decode = True)
 client.watch("data")
 while True:
